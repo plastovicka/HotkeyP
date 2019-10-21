50d951810ea9231289199dffcb3c05a912ce6a8f
Petr Laštovička <plastovicka@users.noreply.github.com>
2019-10-21 19:58:42 +0000

    CHM in Program Files folder can be unblocked by admin

--- source/help.cpp
+++ source/help.cpp
@@ -1,5 +1,5 @@
 /*
- (C) 2008  Petr Lastovicka
+ (C) 2008-2019  Petr Lastovicka
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License.
@@ -11,6 +11,22 @@
 bool chmUnlocked=false;
 const TCHAR *helpClass = _T("HotKeyP_Help");
 //-------------------------------------------------------------------------
+//web browsers attach Zone.Identifier to block every downloaded file
+//blocked CHM file does not show any content, just an empty window
+//we must delete that alternate data stream
+//annoying UAC dialog is required if the app is installed in the Program Files folder
+bool unblockHelp()
+{
+	if (!chmUnlocked) {
+		TCHAR buf[MAX_PATH + 24];
+		getExeDir(buf, lng(13, "help.chm"));
+		_tcscat(buf, _T(":Zone.Identifier:$DATA"));
+		if (!DeleteFile(buf) && GetLastError() == ERROR_ACCESS_DENIED) return true;
+		chmUnlocked = true;
+	}
+	return false;
+}
+//-------------------------------------------------------------------------
 bool showHelp(int anchorId)
 {
 	HWND hHidden = FindWindow(helpClass, 0);
@@ -20,7 +36,7 @@ bool showHelp(int anchorId)
 		//run another hotkeyp.exe process
 		TCHAR buf[MAX_PATH];
 		GetModuleFileName(0, buf, 192);
-		if((int)ShellExecute(0, 0, buf, _T("--htmlhelp"), 0, SW_SHOWNORMAL)<=32){
+		if((int)ShellExecute(0, unblockHelp() ? _T("runas") : 0, buf, _T("--htmlhelp"), 0, SW_SHOWNORMAL)<=32){
 			return false;
 		}
 		//find hidden window which is used to receive commands
@@ -41,22 +57,17 @@ bool showHelp(int anchorId)
 LRESULT helpProc(HWND hWnd, UINT mesg, WPARAM wP, LPARAM lP)
 {
 	static HWND hHH;
+	static int cnt;
 
 	TCHAR buf[MAX_PATH], buf2[MAX_PATH+24];
 	if(mesg==WM_USER+4370){
 		getExeDir(buf, lng(13, "help.chm"));
-		if(!chmUnlocked){
-			_stprintf(buf2, _T("%s:Zone.Identifier:$DATA"), buf);
-			DeleteFile(buf2);
-			chmUnlocked=true;
-		}
 		_stprintf(buf2, _T("%.99s#%d"), lng(30, "main.htm"), wP);
 		hHH = HtmlHelp(0, buf, 0, (!wP) ? 0 : (DWORD_PTR)buf2);
-		SetTimer(hWnd, 11, 1000, 0);
 		return (LPARAM)hHH;
 	}
 	if(mesg==WM_TIMER && wP==11){
-		if(!IsWindow(hHH)) DestroyWindow(hWnd);
+		if(!hHH ? ++cnt>5 : !IsWindow(hHH)) DestroyWindow(hWnd);
 	}
 	if(mesg==WM_DESTROY) PostQuitMessage(0);
 
@@ -70,6 +81,7 @@ int helpProcess()
 
 	readini();
 	initLang();
+	unblockHelp();
 
 	ZeroMemory(&wc, sizeof(wc));
 	wc.lpfnWndProc=(WNDPROC)helpProc;
@@ -79,6 +91,7 @@ int helpProcess()
 
 	HWND hHidden = CreateWindowEx(0, helpClass, _T("Help"), WS_POPUP, 0, 0, 100, 100, 0, 0, inst, 0);
 	if(!hHidden) return 3;
+	SetTimer(hHidden, 11, 1000, 0);
 
 	while(GetMessage(&mesg, NULL, 0, 0)>0){
 		TranslateMessage(&mesg);
